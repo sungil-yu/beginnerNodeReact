f59c54a0f140eb0b2a904e561ec1ee2c2215b173
sungil yu <chinggin@naver.com>
2024-06-04 09:10:54 +0900

    post

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const dev = require('./config/dev')
+const { User } = require('./models/User');
 
+<<<<<<< HEAD
 const dev = require('./config/dev')
 
+=======
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+>>>>>>> dde1149 (post)
 mongoose.connect(uri=dev.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
@@ -13,6 +24,24 @@ mongoose.connect(uri=dev.mongoURI)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/', async (req, res) => {
+
+  try {
+    const user = new User(req.body)
+    const savedUser = await user.save()
+    
+    res.status(200).json({
+      success: true,
+      user: savedUser
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.errmsg
+    })
+  }
 })
 
 app.listen(port, () => {
